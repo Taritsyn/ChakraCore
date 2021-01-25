e9e8bc483ea5561bf8f0e6397238c845e6845d44
Taritsyn <taritsyn@gmail.com>
2021-01-25 20:16:53 +0300

    Refactor a scripts that responsible for creating NuGet packages

--- Build/NuGet/pack.sh
+++ Build/NuGet/pack.sh
@@ -5,31 +5,32 @@
 #-------------------------------------------------------------------------------------------------------
 
 PACKAGE_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PACKAGE_VERSION_FILE="$PACKAGE_ROOT/.pack-version"
-PACKAGE_ARTIFACTS="$PACKAGE_ROOT/Artifacts"
-if [ "$(uname)" = "Darwin" ]; then
-    TARGET_OS_NAME="osx"
-else
-    TARGET_OS_NAME="linux"
-fi
-PACKAGE_NAME="Microsoft.ChakraCore.${TARGET_OS_NAME}-x64"
-TARGET_NUGET_EXE="$PACKAGE_ROOT/nuget.exe"
+PACKAGE_ARTIFACTS_DIR="$PACKAGE_ROOT/Artifacts"
+LOCAL_NUGET_EXE="$PACKAGE_ROOT/nuget.exe"
 
-if [ -d "$PACKAGE_ARTIFACTS" ]; then
+if [ -d "$PACKAGE_ARTIFACTS_DIR" ]; then
     # Delete any existing output.
-    rm $PACKAGE_ARTIFACTS/*.nupkg
+    rm "$PACKAGE_ARTIFACTS_DIR/*.nupkg"
 fi
 
-if [ ! -f "$TARGET_NUGET_EXE" ]; then
-    SOURCE_NUGET_EXE="https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+if [ ! -f "$LOCAL_NUGET_EXE" ]; then
+    NUGET_DIST_URL="https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
 
-    echo "NuGet.exe not found - downloading latest from $SOURCE_NUGET_EXE"
+    echo "NuGet.exe not found - downloading latest from $NUGET_DIST_URL"
 
-    wget -O $TARGET_NUGET_EXE $SOURCE_NUGET_EXE || curl -o $TARGET_NUGET_EXE --location $SOURCE_NUGET_EXE
+    wget -O $LOCAL_NUGET_EXE $NUGET_DIST_URL || curl -o $LOCAL_NUGET_EXE --location $NUGET_DIST_URL
 fi
 
-VERSION_STR=$(<$PACKAGE_VERSION_FILE)
-NUSPEC="$PACKAGE_NAME/$PACKAGE_NAME.nuspec"
+PACKAGE_VERSION_FILE="$PACKAGE_ROOT/.pack-version"
+PACKAGE_VERSION=$(<$PACKAGE_VERSION_FILE)
+
+if [ "$(uname)" = "Darwin" ]; then
+    OS_NAME="osx"
+else
+    OS_NAME="linux"
+fi
+PACKAGE_NAME="Microsoft.ChakraCore.${OS_NAME}-x64"
+PACKAGE_NUSPEC_FILE="$PACKAGE_NAME/$PACKAGE_NAME.nuspec"
 
 # Create new package.
-mono $TARGET_NUGET_EXE pack $NUSPEC -OutputDirectory $PACKAGE_ARTIFACTS -Properties version=$VERSION_STR
+mono $LOCAL_NUGET_EXE pack $PACKAGE_NUSPEC_FILE -OutputDirectory $PACKAGE_ARTIFACTS_DIR -Properties version=$PACKAGE_VERSION
